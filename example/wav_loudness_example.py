ba387300dd5c7135b4668e560807b36990f6885f
lsl666 <lsl666@foxmail.com>
2025-08-13 00:53:49 +0800

    modify example

--- example/wav_loudness_example.py
+++ example/wav_loudness_example.py
@@ -6,9 +6,9 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 from stream_loudness_meter import Ebur128, Ebur128Mode
 
-# 配置参数
-AUDIO_PATH = os.path.expanduser('~/Downloads/testwav/Hotel California-Eagles.wav')  # 修改为你自己的音频路径
-MODE = Ebur128Mode.EBUR128_MODE_I | Ebur128Mode.EBUR128_MODE_HISTOGRAM | Ebur128Mode.EBUR128_MODE_S  # 通常1=EBUR128_MODE_I, 可根据需要调整
+
+AUDIO_PATH = os.path.expanduser('Hotel California-Eagles.wav')  # I like it.
+MODE = Ebur128Mode.EBUR128_MODE_I | Ebur128Mode.EBUR128_MODE_HISTOGRAM | Ebur128Mode.EBUR128_MODE_S
 FRAME_SIZE = 1024
 
 
